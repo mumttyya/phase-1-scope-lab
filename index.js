54353b1d4449f9091062d859595d1c52a002cbea
mumtaza <mumtazmohamed453@gmail.com>
2025-06-22 12:55:09 +0300

    Complete Scuber scope lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName = 'bob'; 
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(newName) {
+  bestCustomer = newName;
+}
+const leastFavoriteCustomer = 'Someone Immutable';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Someone Else';
+}
